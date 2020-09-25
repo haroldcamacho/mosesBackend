9ef084f4e2bb0e5f1496d43c1a64676428664227
haroldcamacho <haroldcj95@gmail.com>
2020-09-24 23:54:51 -0400

    add xmlrpc

--- routes/translations.js
+++ routes/translations.js
@@ -2,6 +2,12 @@ const express = require('express')
 const Translation = require('../models/translation')
 const router = express.Router()
 const validator = require('express-validator')
+const xmlrpc = require ("davexmlrpc");
+const urlEndpointXMLRPC = "http://localhost:8000/RPC2";
+const verb = "";
+const format = "xml"; //could also be "json"
+
+
 //GetTranslations
 
 router.get('/', async(req,res) =>{
@@ -16,19 +22,40 @@ router.get('/', async(req,res) =>{
 
 //Make a translation
 router.post('/', async(req, res)=>{
-    let text = req.body.inputCode;
-    console.log("SSS");
-    //req.translation = new Translation();
-    // await new Promise(r => setTimeout(r, 4000));
-    console.log(req.body.inputCode);
+    req.translation = new Translation();
+    let originalCode = req.body.inputCode;
+    let codeDividedByLine = divideCodeByLine(originalCode);
+    translateCode(codeDividedByLine);
+    // let originalCode = JSON.stringify(req.body.inputCode);
 })
 
 
+function divideCodeByLine(code){
+    return code.trim().split(/\r\n|\r|\n/g);
+}
 
-function processTextToTranslate(){
+function sendXMLRPC(text){
+    let requestMap = new Map();
+    map.set(text,'');
+    xmlrpc.client(urlEndpointXMLRPC, verb, requestMap, format, (err,data) =>{
+        if(err){
+            console.log("err.message ==" + err.message);
+        }
+        else{
+            console.log(JSON.stringify(data));
+            text = JSON.stringify(data);
+        }
+    });
+    return text;
+}
+
+function translateCode(code){
     return async(req, res) =>{
         let translation = req.translation
+        let originalType = req.body.original_code
+        console.log(originalType);
         translation.textToTranslate = req.body.original_code
+
         console.log("this is "+req.body.original_code+"\n")
         // try{
         //     console.log(translation.textToTranslate);
